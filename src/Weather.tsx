244b69590e17f3951514114b77d6a9cfc728c532
lopatop <pavellopato327@gmail.com>
2024-08-11 13:48:02 +0200

    first

---
+++ src/Weather.tsx
@@ -0,0 +1,16 @@
+
+interface WeatherProps {
+    temp: number;
+    description: string;
+}
+
+const Weather = ({ temp, description }:WeatherProps) => {
+    return (
+        <div className="weather">
+            <p>Temperature: {temp} °C</p>
+            <p>Weather: {description}</p>
+        </div>
+    );
+};
+
+export default Weather;
